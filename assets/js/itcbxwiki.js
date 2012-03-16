ac5d6ffb6fc96f1428d2a8c939e0ae569ab4ea4d
itcbx <cbx945@gmail.com>
2012-03-16 22:28:17 +0800

    增加了project euler的一些题目，添加了一个js设置title的值

--- assets/js/itcbxwiki.js
+++ assets/js/itcbxwiki.js
@@ -15,31 +15,38 @@ var itcbxwiki = {
     }
 };
 $(document).ready(function() {
-var syntaxHighlight = function() {
-        itcbxwiki.getCSS('http://alexgorbatchev.com/pub/sh/current/styles/shThemeMidnight.css');
-        $.getScript('http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js', function() {
-            $.getScript('http://alexgorbatchev.com/pub/sh/current/scripts/shAutoloader.js', function(){
-                SyntaxHighlighter.config.strings.expandSource = '+ 展开源码';
-                SyntaxHighlighter.config.strings.viewSource = '新窗口查看源码';
-                SyntaxHighlighter.config.strings.copyToClipboard = '复制到剪贴板';
-                SyntaxHighlighter.config.strings.copyToClipboardConfirmation = '已经复制源码到剪切板';
-                SyntaxHighlighter.config.strings.print = '打印';
-                SyntaxHighlighter.config.strings.help = '';
-                SyntaxHighlighter.config.strings.alert = '语法高亮器&lt;br /&gt;';
-                SyntaxHighlighter.config.strings.noBrush = '找不到下列语言的渲染器: ';
-                SyntaxHighlighter.autoloader(
-				  'python http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPython.js',
-                  'js jscript javascript  http://alexgorbatchev.com/pub/sh/current/scripts/shBrushJScript.js',
-                  'php http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js',
-                  'css CSS Css http://alexgorbatchev.com/pub/sh/current/scripts/shBrushCss.js',
-                  'c cpp http://alexgorbatchev.com/pub/sh/current/scripts/shBrushCpp.js',
-                  'Bash bash http://alexgorbatchev.com/pub/sh/current/scripts/shBrushBash.js'
-                );
-                 
-                SyntaxHighlighter.all();
+    var syntaxHighlight = function() {
+            itcbxwiki.getCSS('http://alexgorbatchev.com/pub/sh/current/styles/shThemeMidnight.css');
+            $.getScript('http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js', function() {
+                $.getScript('http://alexgorbatchev.com/pub/sh/current/scripts/shAutoloader.js', function(){
+                    SyntaxHighlighter.config.strings.expandSource = '+ 展开源码';
+                    SyntaxHighlighter.config.strings.viewSource = '新窗口查看源码';
+                    SyntaxHighlighter.config.strings.copyToClipboard = '复制到剪贴板';
+                    SyntaxHighlighter.config.strings.copyToClipboardConfirmation = '已经复制源码到剪切板';
+                    SyntaxHighlighter.config.strings.print = '打印';
+                    SyntaxHighlighter.config.strings.help = '';
+                    SyntaxHighlighter.config.strings.alert = '语法高亮器&lt;br /&gt;';
+                    SyntaxHighlighter.config.strings.noBrush = '找不到下列语言的渲染器: ';
+                    SyntaxHighlighter.autoloader(
+                                      'python http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPython.js',
+                      'js jscript javascript  http://alexgorbatchev.com/pub/sh/current/scripts/shBrushJScript.js',
+                      'php http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js',
+                      'css CSS Css http://alexgorbatchev.com/pub/sh/current/scripts/shBrushCss.js',
+                      'c cpp http://alexgorbatchev.com/pub/sh/current/scripts/shBrushCpp.js',
+                      'Bash bash http://alexgorbatchev.com/pub/sh/current/scripts/shBrushBash.js'
+                    );
+                     
+                    SyntaxHighlighter.all();
+                });
             });
-        });
     };
     syntaxHighlight();
+
+    (function() {
+        var h1 = $("h1");
+        if(h1 != "") {
+            $("title").text(h1.first().text())
+        }
+    })()
 });
 
